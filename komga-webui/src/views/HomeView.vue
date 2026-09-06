e17ff7a6daca7069b7a9ac8b95efd9439ad27ee6
walid379 <belhoutwalid13@gmail.com>
2026-09-06 10:32:01 +0200

    Change site name

--- komga-webui/src/views/HomeView.vue
+++ komga-webui/src/views/HomeView.vue
@@ -26,7 +26,7 @@
 
         <v-list-item-content>
           <v-list-item-title class="title">
-            Komga
+            BaxterVerse
           </v-list-item-title>
         </v-list-item-content>
 
