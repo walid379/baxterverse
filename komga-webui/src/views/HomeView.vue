4dadf4fc555c7fabc1810eaa9a534055ee0fa5f1
walid379 <114499766+walid379@users.noreply.github.com>
2026-09-08 13:01:54 +0200

    Logo update

--- komga-webui/src/views/HomeView.vue
+++ komga-webui/src/views/HomeView.vue
@@ -139,7 +139,7 @@
             </v-list-item-icon>
 
             <v-list-item-content>
-              <v-list-item-title>Reading Guide</v-list-item-title>
+              <v-list-item-title>{{ $t('readinguide.title') }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
 
